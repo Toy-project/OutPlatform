5d558d6d1d9523c032be967623dddca10d10f1d6
INHOLEE <sayyou0918@gmail.com>
2017-12-15 01:27:03 +0900

    TEST API SERVER

--- server/app.js
+++ server/app.js
@@ -1,14 +1,6 @@
-const express = require('express');
-const path = require('path');
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-// All remaining requests return the React app, so it can handle routing.
-app.get('/', function(request, response) {
-  response.send('hello world');
-});
-
-app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
+var request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
 });
