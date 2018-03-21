200244949ffd6974aa7f7e6a1dbebf33034cd68e
INHOLEE <sayyou0918@gmail.com>
2018-03-21 21:56:31 +0900

    testing cors issue

--- src/services/colleges/index.js
+++ src/services/colleges/index.js
@@ -3,10 +3,11 @@ import axios from 'axios';
 const urlColleges = `${process.env.API_URL}/colleges/`;
 
 export function getColleges(start, end, keyword) {
+  const url = `http://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=3942376907ca20d1edc9bdb7f31c5696&svcType=api&svcCode=SCHOOL&contentType=json&gubun=univ_list&thisPage=${start}&perPage=${end}&searchSchulNm=${keyword}`;
   return axios({
     method: 'get',
     timeout: 20000,
-    url: `${urlColleges}?start=${start}&end=${end}&keyword=${keyword}`,
-    responseType: 'json'
+    url: `${url}`,
   });
+  // url: `${urlColleges}?start=${start}&end=${end}&keyword=${keyword}`,
 }
