476d182e2b25591243b045ae7bddc0c7303439f3
INHOLEE <sayyou0918@gmail.com>
2018-03-22 19:05:54 +0900

    Back-end 서버로 대학API 옮김

--- src/services/colleges/index.js
+++ src/services/colleges/index.js
@@ -3,11 +3,9 @@ import axios from 'axios';
 const urlColleges = `${process.env.API_URL}/colleges/`;
 
 export function getColleges(start, end, keyword) {
-  const url = `http://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=15a88b1d40746ac8defb105695e54c98&svcType=api&svcCode=SCHOOL&contentType=json&gubun=univ_list&thisPage=${start}&perPage=${end}&searchSchulNm=${keyword}`;
   return axios({
     method: 'get',
     timeout: 20000,
-    url: `${url}`,
+    url: `${urlColleges}?start=${start}&end=${end}&keyword=${keyword}`
   });
-  // url: `${urlColleges}?start=${start}&end=${end}&keyword=${keyword}`,
 }
